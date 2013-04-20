ad9fee86a8d8d3f7010f14a9f9ccaff7f128e7e9
nitazou <my@name.example.com>
2013-04-21 00:27:07 +0900

    aaa aaaaaa

---
+++ shiritori.rb
@@ -0,0 +1,36 @@
+# coding: utf-8
+require 'mechanize'
+
+agent = Mechanize.new
+
+word=["ん"]
+
+15.times do |i|
+
+ p i
+ word_last = word[i]
+ word_last = word_last[word_last.length-1]
+
+ if word_last=="ー"
+  word_last = word[i]
+  word_last = word_last[word_last.length-2]
+ end 
+
+ p word_last
+
+
+ suggest=agent.get('http://google.co.jp/complete/search?output=toolbar&hl=ja&q=' + word_last)
+ puts suggest.body.toutf8
+
+ start_text = suggest.body.index("data=""") 
+ end_text = suggest.body.index("""/>") 
+ result=suggest.body[start_text+6..end_text-2].toutf8
+
+ word<<result
+
+ puts word
+
+end
+
+
+
