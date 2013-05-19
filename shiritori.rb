3b75524099914933ede49b615a52ee45a28f9157
nitazou <my@name.example.com>
2013-05-19 21:25:39 +0900

    んがつく候補は採用しない

--- shiritori.rb
+++ shiritori.rb
@@ -28,6 +28,7 @@ def get_last_word word
 end
 
 
+#サジェスト取得
 def get_suggest suggest_q
  #googleから候補をXMLで取得
  agent = Mechanize.new
@@ -43,16 +44,32 @@ def get_suggest suggest_q
  #XMLパースし、読み仮名を取得
  doc.elements.each('toplevel/CompleteSuggestion/suggestion') do |element|
   #p element.attributes["data"]
-   return  element.attributes["data"]
-  break
+   yomi=get_yomi element.attributes["data"]
+   last_word=get_last_word yomi
+   #p yomi
+   end_check=get_end_check last_word
+   if end_check==1
+    
+    else
+    	return element.attributes["data"]
+      break
+   end
  end
-
 end
 
 
+#終わりチェック
+def get_end_check word
+ #puts word
+ if word=="ん"
+ 	return 1
+ else
+ 	return 0
+ end
+end
 
 #初期値
-suggest_list=["test"]
+suggest_list=["魚"]
 suggest_list_kana=[]
 yomi= get_yomi suggest_list[0]
 suggest_list_kana[0] =yomi
@@ -61,7 +78,7 @@ char_set=[*'あ'..'ん']
 
 
 
-21.times do |i|
+50.times do |i|
 
  #p i
  puts suggest_list[i]
@@ -69,14 +86,17 @@ char_set=[*'あ'..'ん']
 
  #サジェスト文字作成
  suggest_q = get_last_word suggest_list_kana[i]
- 
- if suggest_q=="ん"
+
+ end_check=get_end_check suggest_q
+
+ if end_check==1
  	puts "END"
  	break
  end
 
  suggest_result=0
 
+ #過去に出たキーワードをチェック
  loop do
   daburi_flag=0
   suggest_result=get_suggest suggest_q
