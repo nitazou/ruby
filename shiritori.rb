a666c93802fd1957916b57acb63a161a4b0ea09d
nitazou <my@name.example.com>
2013-05-19 14:02:37 +0900

    Mecab XMLパース

--- shiritori.rb
+++ shiritori.rb
@@ -1,46 +1,68 @@
 # coding: utf-8
 require 'mechanize'
+require "rexml/document"
 require 'MeCab'
+require "moji"
 
-def last_word word
+#読み仮名を取得関数
+def get_yomi word
   word.slice! "ー"
-  mecab = MeCab::Tagger.new
-  result = String.new
-  node = mecab.parseToNode word
-  while node.next.next
-    node = node.next
+  mecab = MeCab::Tagger.new("-Oyomi")
 
-    puts "#{node.feature}"
+  katakana= mecab.parse(word)
+  #puts katakana
 
-  end
-  /([^,]*),[^,]*?$/ =~ node.feature
-  $1[-1]
+  hiragana =Moji.kata_to_hira(katakana.toutf8) 
+  
+  hiragana.chomp!
+  return hiragana
+end
 
+#最後の文字（ひらがな）取得関数
+def get_last_word word
+  return word[word.length-1]
 end
 
 agent = Mechanize.new
 
-word=["しりとー"]
+#初期値
+suggest_list=["魚"]
+suggest_list_kana=[]
+yomi= get_yomi suggest_list[0]
+suggest_list_kana[0] =yomi
 
-2.times do |i|
+19.times do |i|
 
- p i
- word_last = last_word word[i]
+ #p i
+ puts suggest_list[i]
+ puts suggest_list_kana[i]
 
- p word_last
+ #サジェスト文字作成
+ suggest_q = get_last_word suggest_list_kana[i]
 
+ #p "q="+suggest_q
 
  #googleから候補をXMLで取得
- suggest=agent.get('http://google.co.jp/complete/search?output=toolbar&hl=ja&q=' + word_last)
+ suggest=agent.get('http://google.co.jp/complete/search?output=toolbar&hl=ja&q=' + suggest_q)
  #puts suggest.body.toutf8
 
- start_text = suggest.body.index("data=""") 
- end_text = suggest.body.index("""/>") 
- result=suggest.body[start_text+6..end_text-2].toutf8
 
- word<<result
+ #XMLを解析する準備
+ source=suggest.body.toutf8
+ doc = REXML::Document.new source
+ #puts doc
 
- puts result
+ #XMLパースし、読み仮名を取得
+ doc.elements.each('toplevel/CompleteSuggestion/suggestion') do |element|
+  #p element.attributes["data"]
+  suggest_list << element.attributes["data"]
+  yomi = get_yomi suggest_list[i+1]
+  #p yomi
+  suggest_list_kana << yomi
+  break
+ end
+
+
  #sleep 1
 
 end
